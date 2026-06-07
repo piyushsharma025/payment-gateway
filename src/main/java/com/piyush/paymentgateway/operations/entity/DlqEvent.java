530f84dba58999c382f560860866c8b3b44c51f3
Piyush Sharma <sharma.piyush8850@gmail.com>
2026-06-08 00:54:18 +0530

    adding few more business entities

---
+++ src/main/java/com/piyush/paymentgateway/operations/entity/DlqEvent.java
@@ -0,0 +1,36 @@
+package com.piyush.paymentgateway.operations.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
+
+@Entity
+@Table(name = "dlq_event")
+public class DlqEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    private WebhookEvent webhookEvent;
+
+    @Column(nullable = false)
+    private UUID merchantId;
+
+    @Column(length = 1000)
+    private String finalError;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(nullable = false, columnDefinition = "jsonb")
+    private Map<String, Object> payload;
+
+    private LocalDateTime movedAt;
+
+    private LocalDateTime replayedAt;
+}
